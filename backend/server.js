1b0b9621cdf86b5f7743aa85c98562c6d5ad8d0a
Álvaro González <202100765@alu.comillas.edu>
2025-12-06 18:23:07 +0100

    Update route handling to serve frontend for all paths

--- backend/server.js
+++ backend/server.js
@@ -141,7 +141,7 @@ app.use('/maps', express.static(mapsDir));
 // --- SERVIR FRONTEND EN PRODUCCIÓN ---
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
